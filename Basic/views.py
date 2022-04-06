506a5353eb62e7705a4f966c9f091b78246486e6
ekdub92 <42200769+ekdub92@users.noreply.github.com>
2022-04-06 12:49:04 +0900

    Update views.py

--- Basic/views.py
+++ Basic/views.py
@@ -4,6 +4,6 @@ import random
 # Create your views here.
 def lotto(request): 
     numbers = []
-    for _ in range(6):
+    for _ in range(7):
         numbers.append(random.randint(1,45))
     return render(request, 'lotto.html', {'numbers' : numbers})
